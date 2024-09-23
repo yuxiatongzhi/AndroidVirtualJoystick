85447ba72392084bcf78ebf88f244f17394bbb63
rick.li <rick.li@semidux.com>
2024-09-23 17:21:44 +0800

    Build<gradle>: 升级 AGP 为 8.4.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
 pluginManagement {
     repositories {
+		maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
+
+        maven { url = uri("https://jitpack.io") }
+	
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -8,6 +14,12 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+		maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
+
+        maven { url = uri("https://jitpack.io") }
+	
         google()
         mavenCentral()
     }
